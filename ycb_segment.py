5bf064a088b55e3413db33bcf103cf0a33b8abbd
alexcbb <alexchapin@hotmail.fr>
2023-05-05 14:10:06 +0200

    Change few things for visualisation

--- ycb_segment.py
+++ ycb_segment.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import torch
 import cv2
+import numpy as np
 
 def bbox_unnormalize(bbox, img_width, img_height):
     x, y, w, h = bbox[0], bbox[1], bbox[2], bbox[3]
@@ -50,7 +51,8 @@ if __name__ == '__main__':
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
         # Predict bounding boxes
-        res = yolo.predict(image)
+        results = yolo.predict(image)
+        results = results[0]
         #boxes = torch.tensor(res[0].boxes)#, device=predictor.device)
 
         # Create figure and axes
@@ -60,13 +62,19 @@ if __name__ == '__main__':
         ax.imshow(image)
 
         # Create a Rectangle patch
-        for val in res[0]:
-            box = val.boxes[0]
+        for obj_ in res[0].boxes:
+            obj = obj_.xywh[0].cpu()
+            color = np.random.rand(3) # generate a random color
+            rect = plt.Rectangle((obj[0], obj[1]), obj[2], obj[3], linewidth=2, edgecolor=color, facecolor='none')
+            ax.add_patch(rect)
+        """ for box in res[0].boxes[0]:
             box_new = box.xywh[0].cpu()
-            rect = patches.Rectangle((box_new[0], box_new[1]), box_new[2], box_new[3], linewidth=1, edgecolor='r', facecolor='none')
+            w, h = box_new[2], box_new[3]
+            x, y = box_new[0]- w /2, box_new[1] - h/2
+            rect = patches.Rectangle((x, y), w, h, linewidth=1, edgecolor='r', facecolor='none')
 
             # Add the patch to the Axes
-            ax.add_patch(rect)
+            ax.add_patch(rect)"""
 
         plt.show()
 
